86084bf4f7e3d52b2d82286b7c7014df8d187e5d
tranhoang130199 <45675855+tranhoang130199@users.noreply.github.com>
2020-05-07 09:17:45 +0700

    load product into home
    load product into home

---
+++ WebApp/Models/ListProduct.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class ListProduct
+    {
+        public List<Product> products { get; set; }
+        public ListProduct()
+        {
+            products = new List<Product>();
+        }
+    }
+}
