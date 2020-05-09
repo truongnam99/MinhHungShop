9bf3de26cb6a59f2ef65bd66e49e77408f8ed35b
tranhoang130199 <45675855+tranhoang130199@users.noreply.github.com>
2020-05-09 15:01:29 +0700

    Revert "Revert "update""
    This reverts commit c9dea0ba67c321cce19d3cab857131442aa2d059.

---
+++ WebApp/Models/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class ProductModel
+    {
+        public Product product { get; set; }
+    }
+}
