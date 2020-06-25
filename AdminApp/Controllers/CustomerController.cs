21d9138fbe914176e00ea62557ee1ed8792cf09b
truongnam <namuit.k12@gmail.com>
2020-06-25 21:24:20 +0700

    Customer, report

---
+++ AdminApp/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogic;
+using DataAccess.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public async Task<ActionResult> Index()
+        {
+            List<Customer> customers = await CustomerBLL.getIns().GetCustomers();
+            ViewBag.customers = customers;
+            return View();
+        }
+    }
+}
---
+++ BusinessLogic/CustomerBLL.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class CustomerBLL
+    {
+        MinhHungShopContext _context = new MinhHungShopContext();
+        private static CustomerBLL Ins;
+        private CustomerBLL()
+        {
+
+        }
+
+        public static CustomerBLL getIns()
+        {
+            if (Ins == null)
+            {
+                Ins = new CustomerBLL();
+            }
+            return Ins;
+        }
+
+        public async Task<List<Customer>> GetCustomers()
+        {
+            List<Customer> customers = await _context.Customer.ToListAsync();
+
+            return customers;
+        }
+    }
+}
